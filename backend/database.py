a85f263c49fdc79d61947d39b91b58d0d1085710
chirag <chiragsinghal2001.cs@gmail.com>
2025-07-24 19:04:53 +0530

    updated database and insites api

--- backend/database.py
+++ backend/database.py
@@ -4,17 +4,16 @@ from motor.motor_asyncio import AsyncIOMotorClient
 # Replace the placeholders below with your actual values
 MONGO_USERNAME = os.environ.get("MONGO_USERNAME", "MONGO_USERNAME")
 MONGO_PASSWORD = os.environ.get("MONGO_PASSWORD", "MONGO_PASSWORD")
-CLUSTER_NAME = os.environ.get("CLUSTER_NAME", "CLUSTER_NAME")
-DB_NAME = "Billing-Db"
 
 MONGO_URL = (
     f"mongodb+srv://{MONGO_USERNAME}:{MONGO_PASSWORD}"
-    f"@{CLUSTER_NAME}.mongodb.net/?retryWrites=true&w=majority&appName=Billing-Data"
+    f"@billing-data.rgnagne.mongodb.net/?retryWrites=true&w=majority&appName=Billing-Data"
 )
 
 client = AsyncIOMotorClient(MONGO_URL)
+DB_NAME = "Billing-Db"
 auth_db = client[DB_NAME]
 users_collection = auth_db["users"]
-database = client["gemini_insights"]
-item_collection = database["bill_items"]
-goal_collection = database["goals"]
+databas = client["gemini_insights"]
+item_collection = databas["bill_items"]
+goal_collection = databas["goals"]
