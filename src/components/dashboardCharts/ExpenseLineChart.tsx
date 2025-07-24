9035b28e12755aeebb71879a6dca10e4238038bf
drushi007 <rushikeshdhurat@gmail.com>
2025-07-24 18:16:07 +0530

    updated with correct json

--- src/components/dashboardCharts/ExpenseLineChart.tsx
+++ src/components/dashboardCharts/ExpenseLineChart.tsx
@@ -1,17 +1,30 @@
-import { LineChart, Line, XAxis, YAxis, Tooltip, CartesianGrid } from 'recharts';
-import { DailyExpense } from './ExpenseTypes';
+import { LineChart, Line, XAxis, YAxis, Tooltip, CartesianGrid, ResponsiveContainer } from 'recharts';
 
+interface DailyExpense {
+  day: string; // YYYY-MM-DD
+  expense: number;
+}
 
 const ExpenseLineChart = ({ data }: { data: DailyExpense[] }) => {
-
   return (
-    <LineChart data={data} width={450} height={300} style={{ margin:'1rem'}}>
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="day" />
-      <YAxis />
-      <Tooltip />
-      <Line type="monotone" dataKey="expense" stroke="#8884d8" strokeWidth={2} />
-    </LineChart>
+    <ResponsiveContainer width="100%" height={300} style={{ margin: '1rem 0' }}>
+      <LineChart data={data}>
+        <CartesianGrid strokeDasharray="3 3" />
+          <XAxis
+            dataKey="day"
+            angle={-45}
+            textAnchor="end"
+            interval={0}
+            height={80}
+            tickFormatter={(date) =>
+              new Date(date).toLocaleDateString('en-US', { month: 'short', day: 'numeric' })
+            }
+          />
+          <YAxis />
+          <Tooltip />
+        <Line type="monotone" dataKey="expense" stroke="#8884d8" strokeWidth={2} />
+      </LineChart>
+    </ResponsiveContainer>
   );
 };
 
